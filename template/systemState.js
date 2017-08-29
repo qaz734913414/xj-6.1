d832f20c3076335cbbd5d61ee0fea6088a936179
JarryMeng <jarrymeng@aliyun.com>
2017-08-29 21:37:30 +0800

    done

--- template/systemState.js
+++ template/systemState.js
@@ -103,8 +103,279 @@ function getTable(){
 			// 			return ++index;
 			// 	}
 			// }
-		]
+		],
+		onClickCell:function(field,value,row){
+			console.log(field)
+			console.log(value)
+			console.log(row)
+			switch(field){
+				case 'usercount':detial(1);break;
+				case 'onlinecount':detial(2);break;
+				case 'retrievecount':detial(3);break;
+				case 'idcardcount':detial(4);break;
+				case 'facecount':detial(5);break;
+				case 'userlogincount':detial(6);break;
+				case 'warncount':detial(7);break;
+			}
+		}
 	});
+}
+function detial(val){
+	var areacodeArr = [],
+    areanameArr = [];
+  var pr = $("#mainSearch input[name='userProvinceId']").val() || '';
+  areacodeArr.push(pr)
+  var ci = $("#mainSearch input[name='userCityId']").val() || '';
+  areacodeArr.push(ci)
+  var di = $("#mainSearch input[name='userDistrictId']").val() || '';
+  areacodeArr.push(di)
+  var prn = $("#mainSearch .province>a").text() || '';
+  areanameArr.push(prn)
+  var cin = $("#mainSearch .city>a").text() || '';
+  areanameArr.push(cin)
+  var din = $("#mainSearch .district>a").text() || '';
+  areanameArr.push(din)
+  var areacode = regk(areacodeArr).substr(1)
+  var areaname = regk(areanameArr).substr(1)
+	var depart=$('#mainSearch #uUnitId').val();
+	var state=val;
+
+			var columns = [];
+			if(state==1){
+				$('#detialModal .modal-title').html('平台用户总量');
+				columns=[
+					{
+						field: 'u_real_name',
+						title: '真实姓名'
+					},
+					{
+						field: 'u_area',
+						title: '地址'
+					},
+					{
+						field: 'u_duty',
+						title: '职务'
+					},
+					{
+						field: 'u_card_id',
+						title: '身份证号'
+					},
+					{
+						field: 'u_phone',
+						title: '手机号'
+					},
+					{
+						field: 'u_telephone',
+						title: '座机号'
+					}
+				]
+			}else if(state==2){
+				$('#detialModal .modal-title').html('在线总量');
+				columns=[
+					{
+						field: 'u_name',
+						title: '真实姓名'
+					},
+					{
+						field: 'u_area',
+						title: '地址'
+					},
+					{
+						field: 'u_duty',
+						title: '职务'
+					},
+					{
+						field: 'u_card_id',
+						title: '身份证号'
+					},
+					{
+						field: 'u_phone',
+						title: '手机号'
+					},
+					{
+						field: 'u_telephone',
+						title: '座机号'
+					}
+				]
+			}else if(state==3){
+				$('#detialModal .modal-title').html('人脸检索总量');
+				columns=[
+					{
+						field: 'u_name',
+						title: '用户名'
+					},
+					{
+						field: 'u_pic_url',
+						title: '图片',
+						formatter: function (value) {
+
+                return '<img style="width:140px;heihgt:170px;" src="' + value + '" />';
+            }
+					},
+					{
+						field: 'u_area',
+						title: '地址'
+					},
+					{
+						field: 'u_duty',
+						title: '职务'
+					},
+					{
+						field: 'u_card_id',
+						title: '身份证号'
+					},
+				]
+			}else if(state==4){
+				$('#detialModal .modal-title').html('身份检索总量');
+				columns=[
+					{
+						field: 'name',
+						title: '用户名'
+					},
+					{
+						field: 'u_pic_url',
+						title: '图片',
+						formatter: function (value) {
+
+                return '<img style="width:140px;heihgt:170px;" src="' + value + '" />';
+            }
+					},
+					{
+						field: 'u_area',
+						title: '地址'
+					},
+					{
+						field: 'u_duty',
+						title: '职务'
+					},
+					{
+						field: 'u_card_id',
+						title: '身份证号'
+					},
+				]
+			}else if(state==5){
+				$('#detialModal .modal-title').html('人脸对比总量');
+				columns=[
+					{
+						field: 'name',
+						title: '用户名'
+					},
+					{
+						field: 'u_pic_url',
+						title: '图片',
+						formatter: function (value) {
+
+                return '<img style="width:140px;heihgt:170px;" src="' + value + '" />';
+            }
+					},
+					{
+						field: 'u_area',
+						title: '地址'
+					},
+					{
+						field: 'u_duty',
+						title: '职务'
+					},
+					{
+						field: 'u_card_id',
+						title: '身份证号'
+					},
+					{
+						field:"persent",
+						title: '相似度'
+					}
+				]
+			}else if(state==6){
+				$('#detialModal .modal-title').html('用户登录次数');
+				columns=[
+					{
+						field: 'u_real_name',
+						title: '真实姓名'
+					},
+					{
+						field: 'u_area',
+						title: '地址'
+					},
+					{
+						field: 'u_duty',
+						title: '职务'
+					},
+					{
+						field: 'u_card_id',
+						title: '身份证号'
+					},
+					{
+						field: 'u_phone',
+						title: '手机号'
+					},
+					{
+						field: 'u_telephone',
+						title: '座机号'
+					}
+				]
+			}else if(state==7){
+
+				$('#detialModal .modal-title').html('告警总量');
+				columns=[
+					{
+						field: 'name',
+						title: '姓名'
+					},
+					{
+						field: 'u_area',
+						title: '地址'
+					},
+					{
+						field: 'u_duty',
+						title: '职务'
+					},
+					{
+						field: 'u_card_id',
+						title: '身份证号'
+					},
+					{
+						field: 'u_phone',
+						title: '手机号'
+					},
+					{
+						field: 'u_telephone',
+						title: '座机号'
+					}
+				]
+			}
+
+
+			$('#detialTable').bootstrapTable("destroy");
+			$('#detialTable').bootstrapTable({
+				url:pathurl + 'syslog/systemstatusDetail?areacode='+areacode+"&departname="+depart+"&state="+state,
+			  method: "post",
+			  contentType: "application/x-www-form-urlencoded",
+			  queryParamsType: " limit",
+			  paginationPreText: "上一页",
+			  paginationNextText: "下一页",
+			  striped: true,
+			  sortable: true,
+			  sortOrder:'asc',
+			  sidePagination:'client',
+			  pagination: true, //是否显示分页（*）
+			  pageNumber: 1, //初始化加载第一页，默认第一页
+			  pageSize: 10, //每页的记录行数（*）
+			  pageList: [
+			    10, 25, 50, 100
+			  ],
+			  paginationDetailHAlign: "left",
+				responseHandler: function (res) {
+						//远程数据加载之前,处理程序响应数据格式,对象包含的参数: 我们可以对返回的数据格式进行处理
+						//在ajax后我们可以在这里进行一些事件的处理
+						return res.result.rows;
+				},
+				columns:columns
+			});
+
+
+			$('#detialModal').modal()
+		// }
+	// })
 }
 function reset(){
   $('#mainSearch #uRoleId').val("");
